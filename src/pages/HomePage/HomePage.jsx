669a7f525d0a6d891c7a744a4abc50c77453761f
Paulfresnel <paul.fresnel1@gmail.com>
2023-03-21 21:05:13 +0100

    Starting Typewriter effect on HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,22 @@ function HomePage(){
         <div className='margined'>
             <h1>ReviewGPT: The Fast Review Generator</h1>
             <br/>
-            <p>For your <br/><br/> <span className="special colored">Events</span>, <br/> <span className="special colored">Appartments</span>,<br/>  <span  className="special colored">Restaurants</span>, <br/>  <span  className="special colored">Stores</span>,<br/>  <span  className="special colored"> Company's physical presence</span></p>
+            <div className='flex'>
+                    <p className='m-bottom bold'>For your  </p>           
+            <div class="typewriter">   
+              <div class="text">
+                <div class="wrapper">
+                  <p className='ta-center'>Restaurants</p>
+                  <p className='ta-center'>Appartments & Hotels</p>
+                  <p className='ta-center'>Museums & Events  </p>
+                </div>
+              </div>
+              </div>
+</div>
+
+
+
+
             <h2 className='ta-left'>Capitalize on your Users</h2>
             <p >Invite your users' to leave some key-words about their feeling and emotions with your business and Quickly generate a relevant full-sentence review </p>
             <div className="flex">
