db423c3c45e980a8d957bd5f609f38ea8f0248cc
Paulfresnel <paul.fresnel1@gmail.com>
2023-03-21 17:29:56 +0100

    Basic CSS & Review Creation Date

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -26,21 +26,31 @@ function Profile(){
             {userProfile.credits < 0 && <p className="btn btn-outline-danger white-font"> You will need to recharge your credits before being able to use our generation tool again</p>}
             
             <br/>
-            <h2>Reviews Generated</h2>
+            {userProfile.reviews && <h2>Reviews Generated</h2>}
             <br/>
+            <div className="flex-wrap">
             {userProfile.reviews && userProfile.reviews.map((review,index)=>{
+                let reviewDate = new Date(review.createdAt)
+                console.log(reviewDate)
+                const options = {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              }
+            let readableUpdatedDate = reviewDate.toLocaleDateString('en-US', options)
                 return <div className="card margined">
-                    <h2>Name: {review.placeName}</h2>
-                    <br/>
-                    <h3><span className="colored">{review.category}</span> Review</h3>
-                    <p>Notes: {review.promptNotes}</p>
-                    <br/>
+                <div className="card-body">
+                    <h2 className="card-title">Name: {review.placeName}</h2> <p>Created the: {readableUpdatedDate}</p>
+                    <h3 className="card-subtitle mb-2 text-muted"><span className="colored">{review.category}</span> Review</h3>
+                    <p className="card-text">Notes: {review.promptNotes}</p>
                     <p>Review generated: </p>
-                    <div className="card margined">
-                    <p>{review.gptResponse}</p>
+                    <div className="margined">
+                    <p className="gpt-response">{review.gptResponse}</p>
+                    </div>
                     </div>
                 </div>
             }) }
+            </div>
         </div>
     )
 }
